80a3b3b078257429b979b29de38a11919a816dd0
Joseph Leon <joseph.leon.9@gmail.com>
2015-03-22 16:30:39 -0500

    Integrate jquery ui drag on li (working), code an onstop event listener.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,32 @@ $(document).ready(function() {
 
   $('#calendar').fullCalendar({
     // put your options and callbacks here
-  })
+    dayClick: function() {
+      console.log('day clicked');
+    },
+    events: [
+      {
+        title: 'My Event',
+        start: '2015-03-03',
+        description: 'I haz an event'
+      }
+    ],
+    //eventRender: function(event, element) {
+      //element.qtip({
+        //content: event.description
+      //});
+    //}
+  });
+
+  $(".dropzone").droppable({
+    accept: ".activated"
+  });
+
+  $( ".activated" ).draggable({
+    addClasses: false,
+    stop: function( event, ui ) {
+      console.log('stopped');
+    },
+  });
 
 });
